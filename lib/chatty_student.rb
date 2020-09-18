357cfe4d62d73803ba4b75ebadce757c595716d3
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-18 14:58:17 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,8 +1,8 @@
-class ChattyStudent < Student 
+class ChattyStudent < Student
 
 
   def hello
-    super 
+    super
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy!
   end
 
